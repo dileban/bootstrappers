5e8dd8d0978686ff01106179a4c2bc229b279012
Dileban Karunamoorthy <dileban@gmail.com>
2018-09-19 09:18:37 +0530

    Add alias to 'git log' to show author and commit dates

--- git-aliases.sh
+++ git-aliases.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 git config --global alias.lol 'log --graph --decorate --pretty=oneline --abbrev-commit --all'
+
+# Show Author and Commit Date, instead of Author Date only.
+git config --global alias.log 'log --pretty=fuller'
