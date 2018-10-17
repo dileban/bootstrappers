f15cba9137eaef2995bd483aba7bfe8f2ac37cb3
Dileban Karunamoorthy <dileban@gmail.com>
2018-10-17 19:51:17 +1100

    Fix alias to log. Change to logf

--- git-aliases.sh
+++ git-aliases.sh
@@ -3,4 +3,4 @@
 git config --global alias.lol 'log --graph --decorate --pretty=oneline --abbrev-commit --all'
 
 # Show Author and Commit Date, instead of Author Date only.
-git config --global alias.log 'log --pretty=fuller'
+git config --global alias.logf 'log --pretty=fuller'
